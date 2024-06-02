bdf36f4a7a58f5ec67209fe552ef0cfe841a5929
HelleFH <hellefruergaard@hotmail.com>
2024-06-02 05:45:13 +0200

    .

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use("/api/v1", mainRouter);
 const port = process.env.PORT || 3001;
 
 const corsOptions = {
-    origin: [' http://localhost:5173', 'https://react-listings-frontend.onrender.com'],
+    origin: [' http://localhost:5173'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     optionsSuccessStatus: 204,
