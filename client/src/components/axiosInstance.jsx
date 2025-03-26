f02e705a3ab86289f95b7a670932a28ff2ebd3d9
HelleFH <142264401+HelleFH@users.noreply.github.com>
2025-03-26 22:03:00 +0100

    Update axiosInstance.jsx

--- client/src/components/axiosInstance.jsx
+++ client/src/components/axiosInstance.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Determine the baseURL based on the environment
 const baseURL = window.location.hostname === 'localhost'
   ? 'http://localhost:3001/api/v1'
-  : 'https://plant-calendar-opoo.onrender.com/api/v1';
+  : 'https://plant-calendar-020a.onrender.com/api/v1';
 
 // Create an instance of Axios with the determined baseURL
 const axiosInstance = axios.create({
